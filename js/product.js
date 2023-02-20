68849e4c83ef9e434f62fba6139e9f5cc0509f53
Oliver <oliver.nicas2303@gmail.com>
2023-02-20 13:18:32 +0100

    js og category

--- js/product.js
+++ js/product.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-fetch("https://madopskrifter-41a3.restdb.io/rest/opskrifter" + id, {
+fetch("https://madopskrifter-41a3.restdb.io/rest/opskrifter", {
   method: "get",
   headers: {
     "x-apikey": "63f32ac7478852088da68490",
@@ -13,11 +13,11 @@ fetch("https://madopskrifter-41a3.restdb.io/rest/opskrifter" + id, {
 function showProduct(product) {
   console.log(product);
 
-  document.querySelector(".product_show h1").textContent = product.productdisplayname;
+  /* document.querySelector(".product_show h1").textContent = product.productdisplayname;
   document.querySelector("span.brandname").textContent = product.brandname;
   document.querySelector("span.category").textContent = product.category;
   document.querySelector("span.articletype").textContent = product.articletype;
   document.querySelector(
     ".product_show img"
-  ).src = `https://madopskrifter-41a3.restdb.io/rest/opskrifter/${product.id}.webp`;
+  ).src = `img/ + https://madopskrifter-41a3.restdb.io/rest/opskrifter?q={"img": ${product.id}.webp`;*/
 }
--- js/productlist.js
+++ js/productlist.js
@@ -1,9 +1,22 @@
 //vi hiver fat i url for at fange en specifik parameter
 const urlParams = new URLSearchParams(window.location.search);
-const category = urlParams.get("category");
+const subcategory = urlParams.get("subcategory");
 let products = [];
 
-fetch(`https://kea-alt-del.dk/t7/api/products?category=${category}&limit=20`)
+let myUrl;
+
+if (subcategory == null) {
+  myUrl = "https://madopskrifter-41a3.restdb.io/rest/opskrifter";
+} else {
+  myUrl = `https://madopskrifter-41a3.restdb.io/rest/opskrifter?q={"subcategory": "${subcategory}"}`;
+}
+
+fetch(myUrl, {
+  method: "get",
+  headers: {
+    "x-apikey": "63f32ac7478852088da68490",
+  },
+})
   .then((response) => response.json())
   .then((data) => {
     //laver et global variabel (alle kan til gå den)
@@ -12,13 +25,13 @@ fetch(`https://kea-alt-del.dk/t7/api/products?category=${category}&limit=20`)
   });
 
 function showProducts() {
-  document.querySelector(".list_grid").innerHTML = "";
+  //document.querySelector(".list").innerHTML = "";
   //looper og kalder showProduct
   products.forEach(showProduct);
 }
-
-document.querySelector("#filter_organize").addEventListener("change", sorting);
-
+/* 
+document.querySelector("#filter_organize").addEventListener("change", sorting); */
+/* 
 function sorting() {
   console.log(this.value);
   // sort by productdisplayname
@@ -53,31 +66,33 @@ function sorting() {
   console.log(products);
   showProducts();
 }
-
+ */
 function showProduct(product) {
+  console.log(product);
   //fang template
-  const template = document.querySelector("#firstView_product_template").content;
+  //const template = document.querySelector("#firstView_product_template").content;
   //lav kopi    true = du vil have alt indhold ("børnene")
-  const copy = template.cloneNode(true);
+  //const copy = template.cloneNode(true);
   //ændre indhold
-  copy.querySelector("h2").textContent = product.productdisplayname;
+  /* copy.querySelector("h2").textContent = product.productdisplayname;
   copy.querySelector(".articletype").textContent = product.articletype;
   copy.querySelector(".brand").textContent = product.brandname;
   copy.querySelector(".price").textContent = `DKK ${product.price},-`;
   copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-  copy.querySelector(".read_more").setAttribute("href", `product.html?id=${product.id}`);
+  copy.querySelector(".read_more").setAttribute("href", `product.html?id=${product.id}`); */
 
   //vegetarisk
+  /* 
   if (product.discount) {
     copy.querySelector(".discount_tag").classList.add("vegetarisk");
     copy.querySelector("p.type").textContent = `Vegetarisk ${(src = "img/")}`;
 
     console.log("added vegetarisk");
-  }
+  } */
 
   //   if (product.soldout) {
   //     copy.querySelector("#status").classList.add(".soldout");
   //   }
   //append (sæt ind)
-  document.querySelector(".list_grid").appendChild(copy);
+  // document.querySelector(".list_grid").appendChild(copy);
 }
