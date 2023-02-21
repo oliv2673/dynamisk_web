9e61257d5163aa6c0d0d8b5eee3e7410ed792b98
Cecilie Nielsen <c.nielsen1507@gmail.com>
2023-02-21 13:51:46 +0100

    product js

--- js/product.js
+++ js/product.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("_id");
 
-fetch("https://madopskrifter-41a3.restdb.io/rest/opskrifter" + id, {
+fetch("https://madopskrifter-41a3.restdb.io/rest/opskrifter/" + id, {
   method: "get",
   headers: {
     "x-apikey": "63f32ac7478852088da68490",
@@ -15,19 +15,19 @@ function showProduct(product) {
   console.log(product);
 
   document.querySelector("h1").textContent = product.titel;
-  document.querySelector(".prepTime").textContent = product.prepTime + "min.";
-  document.querySelector(".cookTime").textContent = product.cookTime + "min.";
-  document.querySelector(".portion").textContent = product.portions + "pers.";
+  document.querySelector(".prepTime").textContent = product.prepTime + " min.";
+  document.querySelector(".cookTime").textContent = product.cookTime + " min.";
+  document.querySelector(".portion").textContent = product.portions + " pers.";
   document.querySelector(".beskrivelse").textContent = product.description;
   document.querySelector(".opskrift_pic").src = `img/${product.img}`;
-  document.querySelector("ol").textContent = product.walkthrough;
+  document.querySelector("ol").innerHTML = product.walkthrough;
 
   product.ingredients.forEach(showIngredients);
 
   function showIngredients(ingrediens){
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
-    copy.querySelector("li").textContent = ingrediens ;
+    copy.querySelector("li span").textContent = ingrediens ;
     document.querySelector(".ingrediens_list").appendChild(copy);
   }
 }
