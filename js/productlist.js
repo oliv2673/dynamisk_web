8352de31cc6fd4d79e9dd1b7043025000c24056b
zasia meincke <zasi0001@kea.dk>
2023-02-23 13:31:28 +0100

    simpel checkbox sorting complete (one at a time)

--- js/productlist.js
+++ js/productlist.js
@@ -61,31 +61,62 @@ function showProducts() {
 
 //tilføj eventlistener på checkboxene
 document.querySelector(".vege_check input").addEventListener("click", selected);
+document.querySelector(".vegan_check input").addEventListener("click", selected);
+document.querySelector(".pesc_check input").addEventListener("click", selected);
 
-function selected(product) {
+function selected() {
   //check value off checkbox
   if (document.querySelector(".vege_check input").checked == true) {
     //document.querySelector(".vegetarisk").classList.add("test");
     //kør loop
-    products.forEach(showSelected);
-  } else {
+    const allVegetarian = document.querySelectorAll(".dish_card");
+    allVegetarian.forEach(showSelected_vege);
+  } /* else {
     document.querySelector(".vegetarisk").classList.remove("test");
+  } */
+
+  if (document.querySelector(".vegan_check input").checked == true) {
+    //kør loop
+    const allVegan = document.querySelectorAll(".dish_card");
+    allVegan.forEach(showSelected_vega);
+  }
+
+  if (document.querySelector(".pesc_check input").checked == true) {
+    //kør loop
+    const allPesc = document.querySelectorAll(".dish_card");
+    allPesc.forEach(showSelected_pesc);
   }
 }
 
-function showSelected(product) {
-  if (product.vegetarian == false) {
+function showSelected_vege(product) {
+  if (product.dataset.vegetarian) {
+    product.classList.remove("dont_show");
+  } else {
+    product.classList.add("dont_show");
+  }
+  /* if (product.vegetarian == false) {
     document.querySelector("article").classList.add("dont_show");
 
     console.log("only vegatarien");
   } else {
     document.querySelector("article").classList.remove("dont_show");
   }
-  console.log("show selected");
+  console.log("show selected"); */
 }
-
-/* 
-document.querySelector("#filter_organize").addEventListener("change", sorting); */
+function showSelected_vega(product) {
+  if (product.dataset.vegan) {
+    product.classList.remove("dont_show");
+  } else {
+    product.classList.add("dont_show");
+  }
+}
+function showSelected_pesc(product) {
+  if (product.dataset.pescetarian) {
+    product.classList.remove("dont_show");
+  } else {
+    product.classList.add("dont_show");
+  }
+}
 
 function showProduct(product) {
   console.log(product);
@@ -105,18 +136,24 @@ function showProduct(product) {
   //vegetarisk
   if (product.vegetarian == true) {
     copy.querySelector(".vegetarisk").classList.remove("dont_show");
+    //tilføj data til et html uden at give den en class
+    copy.querySelector("article").dataset.vegetarian = true;
 
     console.log("added vegetarisk");
   }
   //vegansk
   if (product.vegan == true) {
     copy.querySelector(".vegan").classList.remove("dont_show");
+    //tilføj data til et html uden at give den en class
+    copy.querySelector("article").dataset.vegan = true;
 
     console.log("added vegansk");
   }
   //pescetar
   if (product.pescetarian == true) {
     copy.querySelector(".pescetar").classList.remove("dont_show");
+    //tilføj data til et html uden at give den en class
+    copy.querySelector("article").dataset.pescetarian = true;
 
     console.log("added pescetar");
   }
